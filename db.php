246942a6b3b7042d2949c1b7737708746b3f6360
maubastias <mauri_basty@live.cl>
2024-04-20 13:38:26 -0300

    avance

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'php_crud'
+);
+
+//if (isset($conn)) {
+    //echo 'DB conectada';
+//}
+
+?>
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php include("db.php")?>
+<?php include("includes/header.php")?>
+
+
+<div class="container p-4">
+    <div class="row">
+
+    <div class="col-md-4">
+        <div class="card card-body">
+            <form action="">
+                <div class="form-group">
+                    <input type="text" name="title" class="form-control" placeholder="Tarea" autofocus >
+
+                </div>
+                <div class="form-group"></div> 
+
+            </form>
+
+        </div>
+    </div>
+
+    <div class="col-md-8">
+
+    </div>
+
+    </div>
+
+</div>
+
+<?php include("includes/footer.php")?>
+    
+</body>
+</html>
+
+
